8f9cd3f36d1f42bce5e271ffda3d45c0ff8633ac
bfs <1748160564@qq.com>
2025-05-28 18:41:27 +0800

    项目初始化

---
+++ mojian-api/src/main/java/com/mojian/service/impl/ArticleServiceImpl.java
@@ -0,0 +1,172 @@
+package com.mojian.service.impl;
+
+import cn.dev33.satoken.stp.StpUtil;
+import cn.hutool.core.thread.ThreadUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
+import com.mojian.common.RedisConstants;
+import com.mojian.entity.SysArticle;
+import com.mojian.entity.SysCategory;
+import com.mojian.entity.SysNotifications;
+import com.mojian.service.ArticleService;
+import com.mojian.utils.IpUtil;
+import com.mojian.utils.NotificationsUtil;
+import com.mojian.utils.RedisUtil;
+import com.mojian.vo.article.ArchiveListVo;
+import com.mojian.vo.article.ArticleDetailVo;
+import com.mojian.vo.article.ArticleListVo;
+import com.mojian.vo.article.CategoryListVo;
+import com.mojian.mapper.SysArticleMapper;
+import com.mojian.mapper.SysCategoryMapper;
+import com.mojian.utils.PageUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleServiceImpl implements ArticleService {
+
+    private final SysArticleMapper sysArticleMapper;
+
+    private final SysCategoryMapper sysCategoryMapper;
+
+    private final RedisUtil redisUtil;
+
+    private final NotificationsUtil notificationsUtil;
+
+    @Override
+    public IPage<ArticleListVo> getArticleList(Integer tagId, Integer categoryId, String keyword) {
+        return sysArticleMapper.getArticleListApi(PageUtil.getPage(), tagId, categoryId, keyword);
+    }
+
+    @Override
+    public ArticleDetailVo getArticleDetail(Long id) {
+        ArticleDetailVo detailVo = sysArticleMapper.getArticleDetail(id);
+        // 判断是否点赞
+        Object userId = StpUtil.getLoginIdDefaultNull();
+        if (userId != null) {
+            detailVo.setIsLike(sysArticleMapper.getUserIsLike(id, Integer.parseInt(userId.toString())));
+        }
+
+        // 添加阅读量
+        String ip = IpUtil.getIp();  // 获取当前访问者的IP地址
+        ThreadUtil.execAsync(() -> {  // 使用异步线程执行，避免影响主流程
+            // 从Redis获取所有文章的访问记录
+            Map<Object, Object> map = redisUtil.hGetAll(RedisConstants.ARTICLE_QUANTITY);
+
+            // 获取当前文章的IP访问列表
+            List<String> ipList = (List<String>) map.get(id.toString());
+            // 获取当前文章浏览数
+            LambdaQueryWrapper<SysArticle> queryWrapper = new LambdaQueryWrapper<SysArticle>()
+                    .eq(SysArticle::getId, id)
+                    .select(SysArticle::getQuantity);
+            SysArticle sysArticle = sysArticleMapper.selectOne(queryWrapper);
+            Integer quantity = sysArticle.getQuantity();
+            sysArticle.setQuantity(quantity + 1);
+            LambdaUpdateWrapper<SysArticle> updateWrapper = new LambdaUpdateWrapper<SysArticle>()
+                    .eq(SysArticle::getId, id);
+            if (ipList != null) {  // 如果已有IP列表
+                if (!ipList.contains(ip)) {  // 检查是否已记录当前IP
+                    ipList.add(ip);  // 未记录则添加
+                    // 更新浏览量
+                    sysArticleMapper.update(sysArticle, updateWrapper);
+                }
+            } else {  // 如果没有IP列表
+                ipList = new ArrayList<>();  // 创建新列表
+                ipList.add(ip);  // 添加当前IP
+                // 更新浏览量
+                sysArticleMapper.update(sysArticle, updateWrapper);
+            }
+            // 更新Redis中的记录
+            map.put(id.toString(), ipList);
+            redisUtil.hSetAll(RedisConstants.ARTICLE_QUANTITY, map);
+        });
+        return detailVo;
+    }
+
+    @Override
+    public List<ArchiveListVo> getArticleArchive() {
+
+        List<ArchiveListVo> list = new ArrayList<>();
+
+        List<Integer> years = sysArticleMapper.getArticleArchive();
+        for (Integer year : years) {
+            List<ArticleListVo> articleListVos = sysArticleMapper.getArticleByYear(year);
+            list.add(new ArchiveListVo(year, articleListVos));
+        }
+        return list;
+    }
+
+    @Override
+    public List<CategoryListVo> getArticleCategories() {
+        return sysCategoryMapper.getArticleCategories();
+    }
+
+    @Override
+    public List<ArticleListVo> getCarouselArticle() {
+        return getArticlesByCondition(SysArticle::getIsCarousel);
+    }
+
+    @Override
+    public List<ArticleListVo> getRecommendArticle() {
+        return getArticlesByCondition(SysArticle::getIsRecommend);
+    }
+
+    @Override
+    public Boolean like(Long articleId) {
+        // 判断是否点赞
+        int userId = StpUtil.getLoginIdAsInt();
+        Boolean isLike = sysArticleMapper.getUserIsLike(articleId, userId);
+        if (isLike) {
+            // 点过则取消点赞
+            sysArticleMapper.unLike(articleId, userId);
+        } else {
+            sysArticleMapper.like(articleId, userId);
+            ThreadUtil.execAsync(() -> {
+                //发送通知事件
+                SysNotifications notifications = SysNotifications.builder()
+                        .title("文章点赞通知")
+                        .articleId(articleId)
+                        .isRead(0)
+                        .type("like")
+                        .build();
+                notificationsUtil.publish(notifications);
+            });
+        }
+        return true;
+    }
+
+    @Override
+    public List<SysCategory> getCategoryAll() {
+        return sysCategoryMapper.selectList(new LambdaQueryWrapper<SysCategory>()
+                .orderByAsc(SysCategory::getSort));
+    }
+
+    private List<ArticleListVo> getArticlesByCondition(SFunction<SysArticle, Object> conditionField) {
+        LambdaQueryWrapper<SysArticle> wrapper = new LambdaQueryWrapper<SysArticle>()
+                .select(SysArticle::getId, SysArticle::getTitle, SysArticle::getCover, SysArticle::getCreateTime)
+                .orderByDesc(SysArticle::getCreateTime)
+                .eq(conditionField, 1);
+
+        List<SysArticle> sysArticles = sysArticleMapper.selectList(wrapper);
+
+        if (sysArticles == null || sysArticles.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        return sysArticles.stream().map(item -> ArticleListVo.builder()
+                .id(item.getId())
+                .cover(item.getCover())
+                .title(item.getTitle())
+                .createTime(item.getCreateTime())
+                .build()).collect(Collectors.toList());
+    }
+}
